d54e956c4822746094af37eb8e27d17f6169fbfb
Niklas Bartelheimer <nbartelheimer@cesari.msqc.uni-frankfurt.de>
2024-03-05 17:54:54 +0100

    added likwid profiling

--- include/matrix_ops.hpp
+++ include/matrix_ops.hpp
@@ -15,6 +15,19 @@
 
 #include <getopt.h>
 
+#ifdef LIKWID_PERF
+#include "likwid-marker.h"
+#else
+#define LIKWID_MARKER_INIT
+#define LIKWID_MARKER_THREADINIT
+#define LIKWID_MARKER_SWITCH
+#define LIKWID_MARKER_REGISTER(regionTag)
+#define LIKWID_MARKER_START(regionTag)
+#define LIKWID_MARKER_STOP(regionTag)
+#define LIKWID_MARKER_CLOSE
+#define LIKWID_MARKER_GET(regionTag, nevents, events, time, count)
+#endif
+
 namespace matrix_ops {
 typedef struct {
 	double load_time;
--- src/matrix_ops.cpp
+++ src/matrix_ops.cpp
@@ -1,25 +1,13 @@
 #include "matrix_ops.hpp"
-#include <algorithm>
 
 namespace fs = std::filesystem;
-using MatrixData = std::tuple<fs::path, std::uintmax_t>;
-using MatrixContainer = std::vector<MatrixData>;
+using MatrixContainer = std::vector<std::tuple<fs::path, std::uintmax_t>>;
 using Clock = std::chrono::high_resolution_clock;
 using Duration = std::chrono::duration<double>;
 
 using ValueType = float;
 using DenseMatrix = Eigen::Matrix<ValueType, Eigen::Dynamic, Eigen::Dynamic>;
 using SparseMatrix = Eigen::SparseMatrix<ValueType>;
-
-static bool check = false;
-
-template <typename Iter>
-void sort_matrix_container(Iter begin, Iter end) {
-	std::sort(
-	    begin, end, [](const MatrixData lhs, const MatrixData rhs) -> bool {
-		    return std::get<1>(lhs) < std::get<1>(rhs);
-	    });
-}
 
 void dense_operations(const fs::path in_file,
                       const fs::path out_path,
@@ -36,28 +24,15 @@ void dense_operations(const fs::path in_file,
 	}
 	Duration d = Clock::now() - t0;
 	result.load_time = d.count();
-
 	t0 = Clock::now();
 	LIKWID_MARKER_START("dense");
-	DenseMatrix b = a.array().square();
+	DenseMatrix b = a.inverse();
 	LIKWID_MARKER_STOP("dense");
 	d = Clock::now() - t0;
 
+	std::cout << a << std::endl;
+	std::cout << std::endl << b << std::endl;
 	result.compute_time = d.count();
-
-	if (check) {
-		DenseMatrix c = b.array().sqrt();
-		for (int i = 0; i < c.rows(); ++i) {
-			for (int j = 0; j < c.cols(); ++j) {
-				if (a(i, j) != c(i, j)) {
-					std::cout << "Mismatch at position (" << i << ", " << j
-					          << "): " << a(i, j) << " != " << c(i, j)
-					          << std::endl;
-					std::terminate();
-				}
-			}
-		}
-	}
 
 	t0 = Clock::now();
 	if (!Eigen::saveMarketDense(
@@ -96,28 +71,11 @@ void sparse_operations(const fs::path in_file,
 
 	t0 = Clock::now();
 	LIKWID_MARKER_START("sparse");
-	SparseMatrix b = a.cwiseProduct(a);
+	SparseMatrix b = (a * a);
 	LIKWID_MARKER_STOP("sparse");
 	d = Clock::now() - t0;
 
 	result.compute_time = d.count();
-
-	if (check) {
-		SparseMatrix c = b.cwiseSqrt();
-		Eigen::SparseMatrix<bool> checkResult = c.cwiseEqual(a);
-		for (int k = 0; k < checkResult.outerSize(); ++k) {
-			for (Eigen::SparseMatrix<bool>::InnerIterator it(checkResult, k);
-			     it;
-			     ++it) {
-				if (!it.value()) {
-					std::cerr << "Wrong result\n";
-					std::terminate();
-
-					break;
-				}
-			}
-		}
-	}
 
 	t0 = Clock::now();
 	if (!Eigen::saveMarket(
@@ -144,13 +102,12 @@ int main(int argc, char* argv[]) {
 	fs::path output_dir = fs::current_path();
 	fs::path result_dir = fs::current_path();
 
-	const char* const short_opts = "i:o:r:c";
+	const char* const short_opts = "i:o:r:";
 
 	static const struct option long_opts[] = {
 	    {"input_dir", required_argument, nullptr, 'i'},
 	    {"output_dir", required_argument, nullptr, 'o'},
 	    {"result_dir", required_argument, nullptr, 'r'},
-	    {"check", 0, nullptr, 'c'},
 	};
 
 	while (true) {
@@ -168,11 +125,8 @@ int main(int argc, char* argv[]) {
 			case 'r':
 				result_dir = optarg;
 				break;
-			case 'c':
-				check = true;
-				break;
 			default:
-				std::cerr << "Parameter not supported\n";
+				std::cerr << "Parameter unsupported\n";
 				std::terminate();
 		}
 	}
@@ -195,12 +149,8 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	sort_matrix_container(dense_matrices.begin(), dense_matrices.end());
-	sort_matrix_container(sparse_matrices.begin(), sparse_matrices.end());
-
 	matrix_ops::ResultCollector dense_results(dense_matrices.size());
 	matrix_ops::ResultCollector sparse_results(sparse_matrices.size());
-
 	LIKWID_MARKER_INIT;
 	for (const auto& p : dense_matrices) {
 		dense_operations(std::get<0>(p), output_dir, dense_results);
@@ -210,7 +160,6 @@ int main(int argc, char* argv[]) {
 		sparse_operations(std::get<0>(p), output_dir, sparse_results);
 	}
 	LIKWID_MARKER_CLOSE;
-
 	dense_results.print_csv(result_dir, "dense.csv");
 	sparse_results.print_csv(result_dir, "sparse.csv");
 	return 0;
